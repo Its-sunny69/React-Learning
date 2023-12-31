319592dfe6820e43ba107f332e1b19e361389fdf
Its-sunny69 <115334102+Its-sunny69@users.noreply.github.com>
2023-12-31 21:16:38 +0530

    setup of Redux toolkit

--- 12MegaProject/src/App.jsx
+++ 12MegaProject/src/App.jsx
@@ -1,13 +1,37 @@
+import { useEffect, useState } from 'react'
+import { useDispatch } from "react-redux"
 import './App.css'
+import authservice from './appwrite/auth';
+import { login, logout } from './store/authSlice';
+import { Header, Footer } from './components/index';
 
 function App() {
-  console.log(import.meta.env.VITE_APPWRITE_URL);
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch()
 
-  return (
-    <>
-  <h1>A blog with App write</h1>
-    </>
-  )
+  useEffect(() => {
+    authservice.getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }))
+        } else {
+          dispatch(logout())
+        }
+      })
+      .finally(() => { setLoading(false) })
+  }, [])
+
+  return !loading ? (
+    <div className='h-screen bg-slate-400 content-center mx-auto'>
+      <div className=' w-full block text-center'>
+        <Header />
+        <main>
+          TODO: <outlet />
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) : (null)
 }
 
 export default App
